6bf6939f62e1e9f193548f28876af2ce2b0135d1
pingzing <mcalistern@gmail.com>
2024-01-26 20:32:18 +0200

    Get main page closer to final layout, add shutdown controls

--- LightPadd.Core/Controls/TemplatedControl1.axaml.cs
+++
@@ -1,7 +0,0 @@
-using Avalonia.Controls.Primitives;
-
-namespace LightPadd.Core;
-
-public class TemplatedControl1 : TemplatedControl
-{
-}
--- LightPadd.Core/MainView.axaml.cs
+++ LightPadd.Core/MainView.axaml.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace LightPadd.Core;
@@ -12,8 +14,44 @@ public partial class MainView : UserControl
         InitializeComponent();
     }
 
-    public void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    public void Button_Click(object? sender, RoutedEventArgs e)
     {
         Environment.Exit(0);
+    }
+
+    
+
+    private void FlyoutRestart_Click(object? sender, RoutedEventArgs e)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Debug.WriteLine("Restart button pressed. Ignoring, on dev machine.");
+        }
+
+        if (OperatingSystem.IsLinux())
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "sudo",
+                Arguments = "reboot"
+            });
+        }
+    }
+
+    private void FlyoutShutodwn_Click(object? sender, RoutedEventArgs e)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Debug.WriteLine("Shutdown button pressed. Ignoring, on dev machine.");
+        }
+
+        if (OperatingSystem.IsLinux())
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "sudo",
+                Arguments = "shutdown now"
+            });
+        }
     }
 }
